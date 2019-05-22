9eaa0d3812b1b0894dde4ac13492451a7936c64a
Mitch90 <Mitch90@users.noreply.github.com>
2019-05-22 10:39:44 +0200

    make canvas responsive

--- assets/js/p5/sketch.js
+++ assets/js/p5/sketch.js
@@ -1,6 +1,6 @@
 let angle = 0;
-let angle2= 15; //rotazione quadrato
-var x_off = 1000 ;
+let angle2 = 15; //rotazione quadrato
+var x_off = 1000;
 var y_off = 1000;
 var z_off = 1000;
 var vertices_amount = 150;
@@ -11,72 +11,79 @@ var Z_SPEED = .01; // noise change per frame
 var X_SPEED = .5; // cambiare per aumentare/diminuire blobbing
 var Y_SPEED = .1; // cambiare per aumentare/diminuire blobbing
 
+const $canvas = document.querySelector('#my-canvas'); // find canvas on page
+let canvasWidth, canvasHeight; // store canvas dimensions
+
 
 var color_x, color_speed = -0.05; //velocità con cui cambia il colore
 
-var my_canvas = function(p){
+var my_canvas = function (p) {
 
+  p.setup = function () {
+    var canvas = p.createCanvas(canvasWidth, canvasHeight, p.WEBGL);
+    p.colorMode(p.HSB, 100);
+    color_x = 100; //valore di partenza che decrementa di 0.1 al frame
+    p.ellipseMode(p.RADIUS);
+    // console.log(color_x);
+    canvas.parent('my-canvas');
 
-
-  p.setup = function() {
-  var canvas = p.createCanvas(p.windowWidth/2,p.windowHeight,p.WEBGL);
-  p.colorMode(p.HSB, 100);
-  color_x=100; //valore di partenza che decrementa di 0.1 al frame
-  p.ellipseMode(p.RADIUS);
-  console.log(color_x);
-  canvas.parent('my-canvas');
-
-}
-
-
-p.draw = function(){
-  p.background(100,0);
-  //iniziano i parametri del blob
-  //p.translate(p.width*0.25, 0);
-  p.push();
-
-  p.noStroke();
-  p.fill(color_x, 100,100, 100);
-  if(color_x < 0){
-    color_x=100;
-  } // allo zero, ricomincia il conteggio per fare loop coi colori
-
-
-
-  let w = p.windowWidth;
-  var radius = w/16; // raggio del blob
-  var px_offset = w/9;    // quanto blobba
-  var blob =  p.beginShape();//il blob con 120 vertici
-  for (var a=0; a<p.TWO_PI;a+=p.TWO_PI/vertices_amount) {
-
-    var x = radius*p.sin(a);
-    var y = radius*p.cos(a);
-
-    var new_x = x + ( p.noise(((x_off+x)/NOISE_SCALE),((y_off+y)/NOISE_SCALE), z_off) * px_offset * p.sin(a));
-
-    var new_y = y + (p.noise(((x_off+x)/NOISE_SCALE),((y_off+y)/NOISE_SCALE), z_off) * px_offset * p.cos(a))
-    p.vertex(new_x,new_y);
   }
-  p.endShape();
 
-  p.pop();
 
-  // update il NOISE offsets
-  z_off += Z_SPEED;
-  x_off += X_SPEED;
-  y_off += Y_SPEED;
-  color_x += color_speed;
+  p.draw = function () {
+    p.background(100, 0);
+    //iniziano i parametri del blob
+    //p.translate(p.width*0.25, 0);
+    p.push();
 
-  p.push();
-  // Cubo che gira
-  p.stroke(0,0,0);
-  p.strokeWeight(2);
-  p.noFill();
-  p.rotateY(angle+p.mouseX/200);
-  p.rotateX(p.mouseY/200);
-  p.box(w/5);
-  angle += 0.005;
-  p.pop();
+    p.noStroke();
+    p.fill(color_x, 100, 100, 100);
+    if (color_x < 0) {
+      color_x = 100;
+    } // allo zero, ricomincia il conteggio per fare loop coi colori
+
+
+
+    let w = canvasWidth;
+    var radius = w / 5.4; // raggio del blob
+    var px_offset = w / 4.6;    // quanto blobba
+    var blob = p.beginShape();//il blob con 120 vertici
+    for (var a = 0; a < p.TWO_PI; a += p.TWO_PI / vertices_amount) {
+
+      var x = radius * p.sin(a);
+      var y = radius * p.cos(a);
+
+      var new_x = x + (p.noise(((x_off + x) / NOISE_SCALE), ((y_off + y) / NOISE_SCALE), z_off) * px_offset * p.sin(a));
+
+      var new_y = y + (p.noise(((x_off + x) / NOISE_SCALE), ((y_off + y) / NOISE_SCALE), z_off) * px_offset * p.cos(a))
+      p.vertex(new_x, new_y);
+    }
+    p.endShape();
+
+    p.pop();
+
+    // update il NOISE offsets
+    z_off += Z_SPEED;
+    x_off += X_SPEED;
+    y_off += Y_SPEED;
+    color_x += color_speed;
+
+    p.push();
+    // Cubo che gira
+    p.stroke(0, 0, 0);
+    p.strokeWeight(2);
+    p.noFill();
+    p.rotateY(angle + p.mouseX / 200);
+    p.rotateX(p.mouseY / 200);
+    p.box(w / 2.2);
+    angle += 0.005;
+    p.pop();
   }
 }
-var sketch_1 =new p5(my_canvas);
+if ($canvas != null) {
+  const canvasDimensions = $canvas.getBoundingClientRect();
+  canvasWidth = canvasDimensions.width;
+  canvasHeight = canvasDimensions.height;
+  
+  var sketch_1 = new p5(my_canvas);
+}
